900cdb3afcd9812586c1e6fae3a46ab73681564d
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-06-30 16:47:42 +0500

    Initial Commit

---
+++ CryptoCash/CryptoCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCashApp.swift
+//  CryptoCash
+//
+//  Created by Erkebulan Massainov on 30.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
